81e208abde294c2b2dedb0192d90a442f2e0737f
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-12-08 12:11:23 -0500

    [UPDATE]Navbar done, working

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import homeIcon from '../assets/home-icon.svg'
+import profileIcon from '../assets/profile-icon.svg'
+import plusIcon from '../assets/plus-icon.svg'
 
 export const MenuComponent = () => {
   return (
-    <div>
-      <h1>Menu component</h1>
-    </div>
+    <nav className="menu__container">
+      <Link to='/' >
+        <img src={homeIcon} alt="home link"/>
+      </Link>
+      <Link to='/new-post' >
+        <div className='menu__plus-post'>
+          <img src={plusIcon} alt="new post"/>
+        </div>
+      </Link>
+      <Link to='/profile' >
+        <img src={profileIcon} alt="profile link"/>
+      </Link>
+    </nav>
   )
 }
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -4,14 +4,17 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { MenuComponent } from "../components/MenuComponent";
 import { HomeScreen } from "../screens/HomeScreen";
 import { LoginScreen } from "../screens/LoginScreen";
+import { NewPostScreen } from "../screens/NewPostScreen";
 import { ProfileScreen } from "../screens/ProfileScreen";
 import { RegisterScreen } from "../screens/RegisterScreen";
 
 export default function AppRouter() {
   return (
     <Router>
+      <MenuComponent />
       <div className="router__container">
         <Switch>
           <Route path="/login">
@@ -22,6 +25,9 @@ export default function AppRouter() {
           </Route>
           <Route path="/profile">
             <ProfileScreen />
+          </Route>
+          <Route path="/new-post">
+            <NewPostScreen />
           </Route>
           <Route path="/">
             <HomeScreen />
