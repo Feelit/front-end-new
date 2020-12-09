e50faaa784b4824c2fd0616156e7666cbb1990a6
Nelson Alayon <nelson.alayon@gmail.com>
2020-12-09 12:51:22 -0500

    [ADD] photo card

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import cardImage from '../assets/cardImage.jpg'
+import plusIcon from '../assets/plus-icon.svg'
 
+ 
 export const CardComponent = () => {
   return (
-    <div>
-      <h1>CArd</h1>
+    <div className = "card__container">
+      <figure>
+        <img src={cardImage} alt=""/>
+      </figure>
+      <form action="">
+        <input type="range"/>
+        <h2 className = "card__description">Every day is a travel...</h2>
+        <span className = "card__input-text">
+          <input type="text" className = "card__input-text-text" placeholder = "Let a comment to Ana" />
+          <button className = "card__send-button">
+            <img src={plusIcon} alt="" width = "15px"/>
+          </button>
+        </span>
+      </form>
     </div>
   )
 }
