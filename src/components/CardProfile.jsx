472f57d525f1577e86aa5130623d428a91318ab5
Juan Rios <mrdaniel19996@gmail.com>
2020-12-09 09:54:15 -0500

    [UPDATE]Profile card done

--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 export const CardProfile = () => {
   return (
-    <div>
-      <h1>CArd Profile</h1>
-    </div>
+    <section className='card-profile__container'>
+      <div
+        className='card-profile__img--container'
+        style={{
+          backgroundImage: `url(https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80)`,
+        }}
+      >
+        
+      </div>
+      <input className='card-profile__range' type="range"/>
+      <h2 className='card-profile__title'>Jhon Travolta</h2>
+      <div className='card-profile__list--container'>
+        <ul className='card-profile__list'>
+          <li className='card-profile__list--item'
+            style={{
+              fontSize: `1.5rem`,
+              fontWeight: `bold`
+            }}  
+          >
+            <p>Photos</p>
+            <p>58</p>
+          </li>
+          <li className='card-profile__list--item'>
+            <p>Positive</p>
+            <p>54</p>
+          </li>
+          <li className='card-profile__list--item'>
+            <p>Negative</p>
+            <p>4</p>
+          </li>
+        </ul>
+      </div>
+    </section>
   )
 }
--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { CardProfile } from '../components/CardProfile'
 
 export const ProfileScreen = () => {
   return (
     <div>
       <h1>Profile Screen</h1>
+
+      <CardProfile />
     </div>
   )
 }
