14e9e70aa7ffa9e595833009f532a6c861cff32c
Juan Rios <mrdaniel19996@gmail.com>
2020-12-09 11:43:50 -0500

    [UPDATE]login and register logic working

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AuthLogo } from '../components/AuthLogo';
+import { useForm } from '../hooks/useForm';
+
+
 
 export const LoginScreen = () => {
+
+  const [formLoginValues, handleLoginInputChange] = useForm({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formLoginValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+    //dispatch(startLogin(email, password));
+    //history.replace('/');
+  };
+
+
   return (
     <section className='auth__container'>
       <div className='auth__container--left'>
@@ -10,24 +30,29 @@ export const LoginScreen = () => {
       </div>
       <div className='auth__container--right'>
         <h1 className='auth__title'>Login</h1>
-        <form>
+        <form onSubmit={handleLogin}>
           <label htmlFor="loginEmail">Email</label>
           <input
             className='auth__input'
+            name='email'
             type="email"
             id="loginEmail"
-            placeholder="Tell us your email"/>
+            placeholder="Tell us your email"
+            value={email}
+            onChange={handleLoginInputChange}/>
           <label htmlFor="loginPass">Password</label>
           <input
             className='auth__input'
-            type="password"
-            name="password"
-            id="loginPass"
-            placeholder="Don’t tell your password to anybody"/>
+            type='password'
+            name='password'
+            id='loginPass'
+            placeholder='Don’t tell your password to anybody'
+            value={password}
+            onChange={handleLoginInputChange}/>
           <input
             className='auth__btn'
-            type="button"
-            value="Continue"/>
+            type='submit'
+            value='Continue'/>
           <p> I do not have an account <Link to="/register">Register</Link> </p>
         </form>
       </div>
