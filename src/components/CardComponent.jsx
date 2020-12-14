7ca16eb5e0020929e36f8ab1c5fb2be77475e448
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-12-14 12:18:49 -0500

    [UPDATE]Cards display working

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import plusIcon from '../assets/plus-icon.svg'
 
-export const CardComponent = ({title, photo}) => {
+export const CardComponent = ({title, photo, user}) => {
   return (
-    <div className="card__container">
-      <img src={photo} alt="post" />
-      <form action="">
-        <input type="range" className="card__range" />
-        <h2 className="card__description">{title}</h2>
-        <span className="card__input-text">
-          <input type="text" className="card__input-text-text" placeholder="Let a comment to Ana" />
-          <button className="card__send-button">
-            <img src={plusIcon} alt="" width="15px" />
-          </button>
-        </span>
-      </form>
-    </div>
+    <Link to='/post' className='card__link'>
+      <div className="card__container">
+        <img src={photo} alt="post" />
+        <form action="">
+          <input type="range" className="card__range" />
+          <h2 className="card__description">{title}</h2>
+          <span className="card__input-text">
+            <input type="text" className="card__input-text-text" placeholder={`Let a comment to ${user}`} />
+            <button className="card__send-button">
+              <img src={plusIcon} alt="" width="15px" />
+            </button>
+          </span>
+        </form>
+      </div>
+    </Link>
   )
 }
